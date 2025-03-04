a88dbd53ca0bbc72e63e9b91ea44014917f59789
abhinay chaturvedi <abhinaychaturvedi88@gmail.com>
2025-03-04 10:26:48 +0530

    Implemented ArticulationPoint algo in graph

--- graph/Graph.java
+++ graph/Graph.java
@@ -3,6 +3,7 @@ package graph;
 import java.util.ArrayList;
 import java.util.List;
 
+import graph.articulation_point.ArticulationPoint;
 import graph.bridges_in_graph_tarjan_algo.BridgesInGraph;
 
 public class Graph {
@@ -16,9 +17,20 @@ public class Graph {
         BridgesInGraph bridgesInGraph = new BridgesInGraph(n, list);
         System.out.println(bridgesInGraph.getBridges());
     }
+    private void runArticulationPointAlgo() {
+        int n = 4;
+        List<List<Integer>> list= new ArrayList<>();
+        list.add(new ArrayList<>(List.of(0, 1)));
+        list.add(new ArrayList<>(List.of(1, 2)));
+        list.add(new ArrayList<>(List.of(2, 0)));
+        list.add(new ArrayList<>(List.of(1, 3)));
+        ArticulationPoint articulationPoint = new ArticulationPoint(n, list);
+        System.out.println(articulationPoint.getArticulationPoints());
+    }
     public static void main(String[] args) {
         // n = 4, connections = [[0,1],[1,2],[2,0],[1,3]]
         Graph graph = new Graph();
-        graph.runBridgeAlgo();
+        // graph.runBridgeAlgo();
+        graph.runArticulationPointAlgo();
     }
 }
--- graph/articulation_point/ArticulationPoint.java
+++ graph/articulation_point/ArticulationPoint.java
@@ -1,5 +1,55 @@
 package graph.articulation_point;
 
+import java.util.ArrayList;
+// import java.util.Arrays;
+import java.util.List;
+
 public class ArticulationPoint {
-    
+    private List<Integer> adj[];
+    private int timer;
+    private boolean vis[];
+    private int tin[];
+    private int low[];
+    List<Integer> ans;
+    @SuppressWarnings("unchecked")
+    public ArticulationPoint(int n, List<List<Integer>> edges) {
+        this.timer = 0;
+        vis = new boolean[n];
+        tin = new int[n];
+        low = new int[n];
+        adj = new List[n];
+        for(int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for(List<Integer> edge: edges) {
+            int u = edge.get(0), v = edge.get(1);
+            adj[u].add(v);
+            adj[v].add(u);
+        }
+    }
+    public List<Integer> getArticulationPoints() {
+        ans = new ArrayList<>();
+        dfs(0, -1);
+        return ans;
+    }
+    private void dfs(int u, int par) {
+        vis[u] = true;
+        tin[u] = low[u] = timer++;
+        int children = 0;
+        for(int v: adj[u]) {
+            if(v == par) continue;
+            if(vis[v]) {
+                low[u] = Math.min(low[u], tin[v]);
+            } else {
+                dfs(v, u);
+                children++;
+                low[u] = Math.min(low[u], low[v]);
+                if(low[v] >= tin[u] && par != -1) {
+                    ans.add(u);
+                }
+            }
+        }
+        if(par == -1 && children > 1) ans.add(u);
+    }
+
 }
